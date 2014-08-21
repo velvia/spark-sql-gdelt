8c377091d9d5ea02d6f17181d29ddafec86f500e
Evan Chan <velvia@gmail.com>
2014-08-20 17:45:39 -0700

    Add 2006 to 2012 to downloaded data

--- gdelt_importer.sh
+++ gdelt_importer.sh
@@ -5,5 +5,20 @@ do
   curl -O 'http://data.gdeltproject.org/events/'$i'.zip'
   unzip $i'.zip'
   rm $i'.zip'
+  gzip $i.csv
   sleep 3
+done
+
+for year in `seq 2006 2012`
+do
+  for month in `seq 1 12`
+  do
+    mmzero=0$month
+    mm=${mmzero: -2}
+    curl -O 'http://data.gdeltproject.org/events/'$year$mm'.zip'
+    unzip $year$mm'.zip'
+    rm $year$mm'.zip'
+    gzip $year$mm.csv
+    sleep 3
+  done
 done
